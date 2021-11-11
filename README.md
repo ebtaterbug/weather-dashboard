1de36f6744651d6eae902d268a0b7ed03ff2ffd7
ebtaterbug <ityjoseph@gmail.com>
2021-11-11 14:49:05 -0800

    add forecast data, UV index, and edit readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# weather-dashboard
+# Weather Dashboard
+
+## Description
+The Weather Dashboard application uses a weather API to display the current and forecasted weather of a city. Use the search bar to find a city of your choice. Search history is saved to local storage and displayed, if clicked the past search will re-display to the page. Weather Dashboard currently forecasts 5 days into the future and shows information for temperature, wind speed, humidity and weather icon. The UV index is also displayed for the current day with color.
+
+## Website URL
+
+
+![](assets/images/screenshot.PNG) 
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ function displayWeather(data) {
     $("#temp").text(Math.round((((data.main.temp-273.15)*1.8)+32)) + " °F");
     $("#wind").text(data.wind.speed + " MPH");
     $("#humidity").text(data.main.humidity + " %");
-    $("#uv-index").text(data.wind.speed + " MPH");
 };
+
 
 function getWeather() {
     var city = $('input').val();
@@ -16,6 +16,8 @@ function getWeather() {
         })
         .then(function(data) {
             displayWeather(data);
+            getForecast(data.coord.lat, data.coord.lon);
+            return data
     });
 };
 
@@ -27,6 +29,7 @@ function getWeatherList(city) {
         })
         .then(function(data) {
             displayWeather(data);
+            getForecast(data.coord.lat, data.coord.lon);
     });
 };
 
@@ -58,5 +61,46 @@ function loadData() {
         $(".searches").each(function(i) {
             this.innerHTML = searchList[i];
         })
+    }
+};
+
+function getForecast(lat, lon) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude={part}&appid=b451e261be7a552705f2a22389c1a371";
+    fetch(apiUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            displayForecast(data);
+        })
+};
+
+function displayForecast(data) {
+    $("#uv-index").text(data.current.uvi);
+    
+    for (var i = 1; i < 6; i++) {
+    $('#temp'+[i]).text(Math.round(((((data.daily[i].temp.max)-273.15)*1.8)+32)) + " °F");
+    $('#wind'+[i]).text(data.daily[i].wind_speed);
+    $('#humidity'+[i]).text(data.daily[i].humidity);
+    $('.date'+[i]).text(moment().add([i], 'days').format('L'));
+    $('#icon'+[i]).attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
+    }
+
+    if (data.current.uvi <= 2) {
+        $("#uv-index").addClass("green");
+        $("#uv-index").removeClass("yellow");
+        $("#uv-index").removeClass("red");
+    } 
+    
+    else if (data.current.uvi <= 7) {
+        $("#uv-index").addClass("yellow");
+        $("#uv-index").removeClass("red");
+        $("#uv-index").removeClass("green");
+    }
+    
+    else if (data.current.uvi > 7) {
+        $("#uv-index").addClass("red");
+        $("#uv-index").removeClass("yellow");
+        $("#uv-index").removeClass("green");
     }
 };
